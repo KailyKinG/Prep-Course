4048b01bff6bda5b9850e70b7739d2f6d1db5a2e
KailyKinG_EDK <kailyking@gmail.com>
2022-07-09 20:16:29 -0400

    Todos Pasados En 04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -183,7 +183,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  let count = 0;
+  let i = 0;
+  do{
+      if(arreglo[i] === arreglo[i+1]) count++;
+      i++;
+  }while(i < arreglo.length);
+  return (count === arreglo.length - 1);
 } 
 
 
@@ -192,6 +198,16 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  const meses = [];
+  let count = 0;
+  for(let mes of array){
+      if(mes === "Enero" || mes === "Marzo" || mes === "Noviembre"){
+          meses.push(mes);
+          count++;
+      }
+  }
+  if(count < 3) return "No se encontraron los meses pedidos";
+  return meses;
 }
 
 
@@ -199,6 +215,11 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  const mayor100 = [];
+  for(let i = 0; i < array.length; i++){
+      if(array[i] > 100) mayor100.push(array[i]);
+  }
+  return mayor100;
 }
 
 
@@ -210,6 +231,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  const newValor = [];
+  let count = 0;
+  let i = 0;
+  while(i < 10){
+      numero += 2;
+      if(numero === i) break;
+      newValor.push(numero);
+      count++;
+      i++;
+  }
+  if(count < 10) return "Se interrumpió la ejecución"; 
+  return newValor; 
 }
 
 
@@ -220,6 +253,13 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  const newValor = [];
+  for(let i = 0; i < 10; i++){
+      if(i === 5) continue;
+      numero += 2;
+      newValor.push(numero);
+  }
+  return newValor;
 }
 
 
