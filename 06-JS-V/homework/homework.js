334bd540b3bb2c6448e30e0e8830a691f3e1a449
KailyKinG EDK <kailyking@gmail.com>
2022-07-12 17:17:42 -0400

    Pasados hasta el ejercicio 2 De 06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,40 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+       function Usuario(opciones) {
+         this.usuario = opciones.usuario;
+         this.nombre = opciones.nombre;
+         this.email = opciones.email;
+         this.password = opciones.password;
+       }
+
+       Usuario.prototype.saludar = function () {
+         return "Hola, mi nombre es " + this.nombre;
+       };
+       return Usuario;
 }
+
+/*
+  const Usuario = crearUsuario();
+
+  let user1 = new Usuario({usuario: "FT-29c__Dosk", nombre:"KailyKinG", email: "example@example.com", password: "12345678910K"});
+  user1;
+
+  let user2 = new Usuario({usuario: "Camaleon", nombre: "Lyna", email: "example@example.com", password: "67122433K"});
+  console.log(user2);
+  console.log(user2.saludar());
+*/
+
+
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function(){
+   return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
